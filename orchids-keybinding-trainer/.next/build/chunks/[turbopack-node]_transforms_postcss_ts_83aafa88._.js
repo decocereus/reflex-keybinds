fff2ee30de4f05c3b61bd8cf65d851ff13f4c48e
decocereus <amartyasinghkings07@gmail.com>
2025-12-14 12:17:02 +0530

    Initial sync after enabling

---
+++ orchids-keybinding-trainer/.next/build/chunks/[turbopack-node]_transforms_postcss_ts_83aafa88._.js
@@ -0,0 +1,12 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/orchids-projects/orchids-keybinding-trainer/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "build/chunks/[root-of-the-server]__fc1e645c._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/orchids-projects/orchids-keybinding-trainer/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
